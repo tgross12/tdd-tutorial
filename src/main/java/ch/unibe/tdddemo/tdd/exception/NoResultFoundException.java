6c855b7c788388f7b53952127b359b31296d4cc3
Timm Gross <timm.gross@id.unibe.ch>
2020-01-24 13:41:33 +0100

    fix

--- src/main/java/ch/unibe/tdddemo/tdd/exception/NoResultFoundException.java
+++ src/main/java/ch/unibe/tdddemo/tdd/exception/NoResultFoundException.java
@@ -1,3 +1,8 @@
 package ch.unibe.tdddemo.tdd.exception;
 
-public class NoResultFoundException extends Exception {}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Not Found")
+public class NoResultFoundException extends Exception {
+}
