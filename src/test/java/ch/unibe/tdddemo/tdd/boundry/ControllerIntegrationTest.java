303130cb9c0fed1f86463487050b6f12e8dddca8
Timm Gross <timm.gross@id.unibe.ch>
2020-01-31 15:45:30 +0100

    tests verify status codes

--- src/test/java/ch/unibe/tdddemo/tdd/boundry/ControllerIntegrationTest.java
+++ src/test/java/ch/unibe/tdddemo/tdd/boundry/ControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,6 +24,7 @@ public class ControllerIntegrationTest {
   public void getUnisportByPersonalNrIntegrationTest() throws Exception {
     String url = "http://localhost:" + port + "/employmentbyguid?guid=cbtestguid";
     ResponseEntity result = restTemplate.getForEntity(url, String.class);
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(result.getBody())
         .isEqualTo(
             "[{\"firstName\":\"Camillo\",\"lastName\":\"Berneri\",\"departmentName\":\"God Dept\",\"roleTypeName\":\"Employee\",\"beginDate\":\"2010-10-10\",\"endDate\":\"2015-10-17\"},{\"firstName\":\"Camillo\",\"lastName\":\"Berneri\",\"departmentName\":\"God Dept\",\"roleTypeName\":\"Lecturer\",\"beginDate\":\"2008-08-17\",\"endDate\":\"2020-12-01\"}]");
@@ -32,6 +34,7 @@ public class ControllerIntegrationTest {
   public void getUnisportByShibIdExceptionTest() throws Exception {
     String url = "http://localhost:" + port + "/employmentbyguid?guid=notexistingguid";
     ResponseEntity result = restTemplate.getForEntity(url, String.class);
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     assertThat(result.toString()).contains("Not Found");
   }
 }
